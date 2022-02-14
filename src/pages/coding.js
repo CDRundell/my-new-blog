528192ea0c7984401b9889e386bb5fd011c8950c
Chris Rundell <cdrundell@icloud.com>
2022-02-14 18:43:35 +0000

    generated site

---
+++ src/pages/coding.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+
+const CodingPage = () => {
+    return <div>
+            <h1> "Welcome to my coding page!" </h1>
+          </div >
+}
+
+export default CodingPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,17 @@
 import React from "react"
+import { Link  } from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <body>
+            <div class="title">
+              <h1 class="title-header">Homepage</h1>
+            </div>
+
+            <div class="tiles">
+              <div class="first-post">
+                <h2 class="tile-font"><Link to="/coding"> Learning to code 💻</Link></h2>
+                <h3 class="tile-font-medium">Documenting my experience from chemist to coder</h3>
+              </div>
+            </div>
+          </body>
 }
