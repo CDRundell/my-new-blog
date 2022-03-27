3b55f34b99b83f07a4bd9a4bc82f43a91cec380e
Chris Rundell <cdrundell@icloud.com>
2022-03-27 21:51:58 +0100

    helmet completed for all pages

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -1,7 +1,9 @@
 import React from 'react'
-import Layout from '../components/layout'
 import { graphql } from "gatsby"
 import { Link } from "gatsby"
+import { Helmet } from 'react-helmet'
+
+import Layout from '../components/layout'
 
 export const query = graphql`
   query($slug: String!) {
@@ -16,6 +18,7 @@ export const query = graphql`
 const BlogPost = (props) => {
   return (
     <Layout>
+      <Helmet title={props.data.markdownRemark.frontmatter.title} />
       <h1>{props.data.markdownRemark.frontmatter.title}</h1>
       <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }} />
       <Link to="/blog">Back to blog page</Link>
