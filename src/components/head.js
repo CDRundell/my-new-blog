82424d65b8657baeaa4affdc59b075fad137623f
Chris Rundell <cdrundell@icloud.com>
2022-04-25 21:14:09 +0100

    meta tag description added

--- src/components/head.js
+++ src/components/head.js
@@ -14,7 +14,11 @@ const Head = ( { title } ) => {
     `)
 
   return (
-    <Helmet title={`${title} | ${data.site.siteMetadata.title}`} />
+    <Helmet title={`${title} | ${data.site.siteMetadata.title}`} >
+      <meta charSet="utf-8" />
+      <meta name="description" content="organic chemist learning to code" />
+      <meta name="og:description" content="organic chemist learning to code" />
+    </Helmet>
   )
 }
 
